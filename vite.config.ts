fdbe893c995b5fa6b7891ea43559141b9825a8e7
VikumKarunathilake <vikumbk@yahoo.com>
2024-11-10 16:51:30 +0530

    Adds Firebase and Vercel Postgres dependencies
    Adds necessary dependencies for Firebase authentication and Vercel Postgres database integration.
    
    Updates `package-lock.json` with new packages and versions.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'https://flux-api.up.railway.app',
+        target: 'https://gallery.api.elixircraft.net/',
         changeOrigin: true,
       },
     },
